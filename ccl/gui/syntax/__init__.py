ba140dfc4ccca14bd46d88b620f6be4db81d38fd
Tomáš Raček <tom@krab1k.net>
2019-05-29 15:01:49 +0200

    Remove Python generator as there is no suitable backend

--- ccl/gui/syntax/__init__.py
+++ ccl/gui/syntax/__init__.py
@@ -1,6 +1,5 @@
 """Syntax highlighting for CCL and backend langugages"""
 
 from .common import SyntaxHighlighter
-from .python import PythonHighlighter
 from .ccl import CCLHighlighter
 from .latex import LatexHighlighter
